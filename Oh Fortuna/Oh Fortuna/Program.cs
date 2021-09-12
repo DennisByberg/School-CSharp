842b18fc6c859fbf054c2e020d34a5ccdd99818f
Dennis Byberg <dennis.byberg@hotmail.com>
2021-09-12 14:48:09 +0200

    edit

---
+++ Oh Fortuna/Oh Fortuna/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Oh_Fortuna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
