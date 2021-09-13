55723519ea18d477e0e27e6f275840917ddbbf5a
Dennis Byberg <dennis.byberg@hotmail.com>
2021-09-13 15:00:31 +0200

    Klart att användas!

---
+++ Långbord/Långbord/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Långbord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många bord har ni?");
+
+            int.TryParse(Console.ReadLine(), out int svar);
+
+            Console.WriteLine(antalPlatser(svar));
+        }
+
+         static int antalPlatser(int antalBord)
+        {
+            int antalSidoplatser = 2;
+            int antalKantplatser = 2;
+            int antalSittplatser = antalBord * antalSidoplatser + antalKantplatser;
+            return antalSittplatser;
+        }
+    }
+}
