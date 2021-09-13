43379e480785ca9162772ca99df2542a7d7574a9
Dennis Byberg <dennis.byberg@hotmail.com>
2021-09-13 15:47:51 +0200

    hmm...

--- Tågresa/Tågresa/Program.cs
+++ Tågresa/Tågresa/Program.cs
@@ -6,7 +6,18 @@ namespace Tågresa
     {
         static void Main(string[] args)
         {
-         ...  
+            DateTime idag = DateTime.Now;
+            int startTimme = 8;
+            int startMinut = 24;
+            int frammeTimme = 11;
+            int frammeMinut = 33;
+
+            DateTime start = new DateTime(idag.Year, idag.Month, idag.Day, startTimme, startMinut, 00);
+            DateTime framme = new DateTime(idag.Year, idag.Month, idag.Day, frammeTimme, frammeMinut, 00);
+            TimeSpan resTid = framme - start;
+            // Skriv ut resultatet
+            Console.WriteLine($"Resan tar {resTid.Hours:00}:{resTid.Minutes:00}");
+
         }
     }
 }
