20ebbbcd46f9cce2fce50a70dcb6e62fefa35bc6
Dennis Byberg <dennis.byberg@hotmail.com>
2021-09-16 01:27:59 +0200

    Klar!

---
+++ HundOchKattÅr/HundOchKattÅr/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HundOchKattÅr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int HumanAge;
+            int DogAge;
+
+            Console.Write("Hur gammal är du?: ");
+            HumanAge = Convert.ToInt32(Console.ReadLine());
+
+            DogAge = (HumanAge * 7);
+
+            Console.WriteLine($"Du är {HumanAge} år och i Hund/Kattår är du {DogAge}");
+        }
+    }
+}
