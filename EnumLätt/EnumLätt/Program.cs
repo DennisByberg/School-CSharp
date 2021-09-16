c4a9158625f462be6cde16ba7608895360bc0d3c
Dennis Byberg <dennis.byberg@hotmail.com>
2021-09-16 13:31:38 +0200

    20% Klar

---
+++ EnumLätt/EnumLätt/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EnumLätt
+{
+    class Program
+    {
+        static void Main(string[] args)
+           
+        {
+
+        }
+
+    }
+}
---
+++ EnumLätt/EnumLätt/Veckodagar/Veckodagar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumLätt.Veckodagar
+{
+    class Veckodagar
+    {
+        enum VeckoDagar
+        {
+            Måndag = 1,
+            Tisdag,
+            Onsdag,
+            Torsdag,
+            Fredag,
+            Lördag,
+            Söndag
+        }
+    }
+}
