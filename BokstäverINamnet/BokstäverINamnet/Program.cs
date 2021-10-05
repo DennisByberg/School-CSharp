e8ca4b4ab9826a4b4b806cd7254b04629e97cd18
Dennis Byberg <dennis.byberg@hotmail.com>
2021-10-05 10:19:57 +0200

    Klar!

---
+++ BokstäverINamnet/BokstäverINamnet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BokstäverINamnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad heter du?");
+            string namn = Console.ReadLine();
+
+            Console.WriteLine("Ditt namn är " + namn.Length +" bokstäver långt!");
+        }
+    }
+}
