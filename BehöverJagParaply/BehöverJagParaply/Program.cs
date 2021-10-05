d99cb3edf9875044b5b3e0dcd824767e6e3547f5
Dennis Byberg <dennis.byberg@hotmail.com>
2021-10-05 10:39:56 +0200

    Klar!

---
+++ BehöverJagParaply/BehöverJagParaply/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BehöverJagParaply
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variabler.
+            bool rain = false;
+            bool willRain = true;
+
+            bool needUmbrella = rain || willRain;
+
+            Console.WriteLine("Behöver jag paraply?");
+            Console.WriteLine(needUmbrella);
+        }
+    }
+}
