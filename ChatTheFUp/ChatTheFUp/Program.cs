75957266a544ae52b8916fb53b199b858b4ab749
Dennis Byberg <dennis.byberg@hotmail.com>
2021-10-09 14:15:24 +0200

    Person klass påbörjat!

---
+++ ChatTheFUp/ChatTheFUp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChatTheFUp
+{
+    class Program
+    {
+        class Person
+        {
+            public string name { get; protected set; }
+            public int age { get; protected set; }
+
+            public Person(string name, int age)
+            {
+                this.name = name;
+                this.age = age;
+            }
+
+            public string returnDetails()
+            {
+                return name + " - " + age;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Person person = new Person("Dennis", 23);
+
+            Console.WriteLine(person.returnDetails());
+
+            Console.ReadLine();
+        }
+    }
+}
